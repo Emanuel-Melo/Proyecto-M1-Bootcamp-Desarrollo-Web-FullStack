746d665bdc209a30ff7912c3679ec381ebf4c458
Emanuel <emanuel.florez1016@gmail.com>
2026-03-20 09:55:03 -0500

    Se agregó función que pinta la primera celda, se arreglaron unos inconvenientes con los id en html, y se generó respuesta en la consola de la página para revisar que todo funcione

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const boton = document.getElementById("generar");
 const colores = document.querySelectorAll(".color");
 const select = document.getElementById("cantidad");
 const modos = document.getElementsByName("mode");
+
+boton.addEventListener("click", function() {
+    const color = generarColorHEX();
+    colores[0].style.backgroundColor = color;
+});
+
+function generarColorHEX() {
+    const caracteres = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += caracteres[Math.floor(Math.random() * 16)];
+    }
+    
+    return color;
+}
+
+console.log(generarColorHEX());
