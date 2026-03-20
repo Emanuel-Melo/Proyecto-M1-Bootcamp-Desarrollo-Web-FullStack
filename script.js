df375e2d25d32f93d57b63f83f59ed5471d1e4d8
Emanuel <emanuel.florez1016@gmail.com>
2026-03-19 19:15:47 -0500

    Se agregaron las constantes en el archivo js

---
+++ script.js
@@ -0,0 +1,4 @@
+const boton = document.getElementById("generar");
+const colores = document.querySelectorAll(".color");
+const select = document.getElementById("cantidad");
+const modos = document.getElementsByName("mode");
